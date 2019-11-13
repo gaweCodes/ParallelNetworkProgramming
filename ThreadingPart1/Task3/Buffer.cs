0f13fe8413416a1bda1193b111060431fbfdbcf6
Gabriel Weibel <g.weibel@rey-automation.ch>
2019-11-13 15:59:09 +0100

    setup

---
+++ ThreadingPart1/Task3/Buffer.cs
@@ -0,0 +1,22 @@
+﻿namespace Task3
+{
+    public class Buffer
+    {
+        private const int Size = 4;
+        private readonly char[] _buf = new char[Size];
+        private int _head, _tail;
+
+        public void Put(char ch)
+        {
+            _buf[_tail] = ch;
+            _tail = (_tail + 1) % Size;
+        }
+
+        public char Get()
+        {
+            var ch = _buf[_head];
+            _head = (_head + 1) % Size;
+            return ch;
+        }
+    }
+}
---
+++ ThreadingPart1/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Task3
+{
+    internal class Program
+    {
+        [ThreadStatic] private static readonly Buffer Buffer;
+        private static void Main()
+        {
+
+            var writer = new Thread(PutBuffer);
+            writer.Start();
+            var reader = new Thread(ReadBuffer);
+            reader.Start();
+        }
+
+        private static void PutBuffer()
+        {
+            var r = new Random();
+            do
+            {
+                Buffer.Put(Console.ReadLine()[0]);
+                Thread.Sleep(r.Next(1, 5000));
+            } while (1 == 1);
+        }
+        private static void ReadBuffer()
+        {
+            var r = new Random();
+            do
+            {
+                Console.Write(Buffer.Get());
+                Thread.Sleep(r.Next(1, 5000));
+            } while (1 == 1);
+        }
+    }
+}
