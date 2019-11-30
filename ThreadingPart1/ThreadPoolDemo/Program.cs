220d06fce2a7ad91a11d600b423c1a2105dfebf5
Gabriel Weibel <gabriel.weibel@hotmail.de>
2019-11-30 13:39:56 +0100

    Added Threading things

---
+++ ThreadingPart1/ThreadPoolDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    delegate int AdditionDelegate(int number1, int number2);
+    internal class Program
+    {
+        private static void Main()
+        {
+            AdditionDelegate del = DoWork;
+            // Console.WriteLine($"Without begin invoke {del(10, 20)}");
+            Console.WriteLine("Starting with BeginInvoke");
+            var result = del.BeginInvoke(10,20, AddCallback, null);
+            Console.WriteLine("Waiting on work...");
+            var ret = del.EndInvoke(result);
+            Console.WriteLine($"{ret}");
+            Console.ReadLine();
+        }
+
+        private static int DoWork(int number1, int number2)
+        {
+            Thread.Sleep(2000);
+            return number1 + number2;
+        }
+        private static void AddCallback(IAsyncResult result) => Console.WriteLine($"Callback {result.IsCompleted}");
+    }
+}
