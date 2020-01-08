e315af766fa22e1ee775c40b4eb459f42cce58b5
Gabriel Weibel <g.weibel@rey-automation.ch>
2020-01-08 11:57:23 +0100

    Exception Handling

---
+++ ThreadingPart1/ExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var t = Task<int>.Factory.StartNew(() =>
+            {
+                Thread.Sleep(3000);
+                throw new NotImplementedException();
+            });
+            try
+            {
+                var x = t.Result;
+                Console.WriteLine(x);
+            }
+            catch (AggregateException ae)
+            {
+                ae.Flatten().Handle(e =>
+                {
+                    if (!(e is NotImplementedException)) return false;
+                    Console.WriteLine("Exception handling");
+                    Console.ReadLine();
+                    return true;
+                });
+            }
+        }
+    }
+}
