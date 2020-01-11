be22415a261f8b1887b6553d5e64ba1b2d505b85
Gabriel Weibel <gabriel.weibel@hotmail.de>
2020-01-11 18:17:13 +0100

    Aufgaben

---
+++ ThreadingPart1/AsyncParallelProgramming/GameOfLife.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncParallelProgramming
+{
+    internal class GameOfLife
+    {
+        public int N;
+        public int[,] NewBoard()
+        {
+            var board = new int[N, N];
+            var rand = new Random();
+            for (var i = 0; i < N; i++)
+            {
+                for (var j = 0; j < N; j++)  board[i, j] = rand.NextDouble() > 0.5 ? 1 : 0;
+            }
+            return board;
+        }
+        public void Print(int[,] board)
+        {
+            for (var i = 0; i < N; i++)
+            {
+                for (var j = 0; j < N; j++)  Console.Write(board[i, j] == 1 ? "*" : ".");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public int Neighbors(int[,] board, int i, int j)
+        {
+            var top = i == 0 ? N - 1 : i - 1;
+            var bot = i == N - 1 ? 0 : i + 1;
+            var left = j == 0 ? N - 1 : j - 1;
+            var right = j == N - 1 ? 0 : j + 1;
+            return board[top, left] + board[top, j] + board[top, right]
+                   + board[i, left] + board[i, right]
+                   + board[bot, left] + board[bot, j] + board[bot, right];
+        }
+        public int[,] NextGeneration(int[,] board)
+        {
+            var newBoard = new int[N, N];
+            Parallel.For(0, N, i => {
+                Parallel.For(0, N, j => {
+                    var neighbors = Neighbors(board, i, j);
+                    if (board[i, j] == 1) newBoard[i, j] = neighbors < 2 || neighbors > 3 ? 0 : 1;
+                    else newBoard[i, j] = neighbors == 3 ? 1 : 0;
+                });
+            });
+            return newBoard;
+        }
+    }
+}
---
+++ ThreadingPart1/AsyncParallelProgramming/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncParallelProgramming
+{
+    internal class Program
+    {
+        private static readonly int delay = 100;
+        private static void Main()
+        {
+            var x = new GameOfLife {N = 1000};
+            var board = x.NewBoard();
+            x.Print(board);
+            const int generations = 1;
+            for (var i = 0; i < generations; i++)
+            {
+                board = x.NextGeneration(board);
+                x.Print(board);
+            }
+
+            return;
+            var sw1 = new Stopwatch();
+            var sw2 = new Stopwatch();
+            var sw3 = new Stopwatch();
+            var sw4 = new Stopwatch();
+
+            sw1.Start();
+            DoAsync();
+            sw1.Stop();
+
+            sw2.Start();
+            DoParallel();
+            sw2.Start();
+            
+            sw3.Start();
+            DoNormal();
+            sw3.Stop();
+
+            sw4.Start();
+            DoBoth();
+            sw4.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine($"Async: {sw1.ElapsedMilliseconds} Parallel: {sw2.ElapsedMilliseconds} Normal: {sw3.ElapsedMilliseconds} Both: {sw4.ElapsedMilliseconds}");
+        }
+        private static void DoBoth()
+        {
+            try
+            {
+                var dir = new DirectoryInfo(@"C:\Users\Gabriel\source\repos\GitHub\ParallelNetworkProgramming");
+                var files = dir.GetFiles("*.cs", SearchOption.AllDirectories);
+                Console.WriteLine("Files in " + dir.FullName + " containing 'Using'");
+                Parallel.ForEach(files, async file => {
+                    var r = new StreamReader(file.FullName);
+                    var s = await r.ReadToEndAsync();
+                    Thread.Sleep(delay);
+                    if (s.IndexOf("Using", StringComparison.Ordinal) >= 0) Console.WriteLine(" " + file.Name);
+                });
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("-- invalid directory name");
+            }
+        }
+        private static void DoNormal()
+        {
+            try
+            {
+                var dir = new DirectoryInfo(@"C:\Users\Gabriel\source\repos\GitHub\ParallelNetworkProgramming");
+                var files = dir.GetFiles("*.cs", SearchOption.AllDirectories);
+                Console.WriteLine("Files in " + dir.FullName + " containing 'Using'");
+                foreach(var file in files) 
+                {
+                    var r = new StreamReader(file.FullName);
+                    var s = r.ReadToEnd();
+                    Thread.Sleep(delay);
+                    if (s.IndexOf("Using", StringComparison.Ordinal) >= 0) Console.WriteLine(" " + file.Name);
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("-- invalid directory name");
+            }
+        }
+        private static void DoParallel()
+        {
+            try
+            {
+                var dir = new DirectoryInfo(@"C:\Users\Gabriel\source\repos\GitHub\ParallelNetworkProgramming");
+                var files = dir.GetFiles("*.cs", SearchOption.AllDirectories);
+                Console.WriteLine("Files in " + dir.FullName + " containing 'Using'");
+                Parallel.ForEach(files, file => {
+                    var r = new StreamReader(file.FullName);
+                    var s = r.ReadToEnd();
+                    Thread.Sleep(delay);
+                    if (s.IndexOf("Using", StringComparison.Ordinal) >= 0) Console.WriteLine(" " + file.Name);
+                });
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("-- invalid directory name");
+            }
+        }
+        private static void DoAsync()
+        {
+            try
+            {
+                var dir = new DirectoryInfo(@"C:\Users\Gabriel\source\repos\GitHub\ParallelNetworkProgramming");
+                var files = dir.GetFiles("*.cs", SearchOption.AllDirectories);
+                var t = SearchFilesAsync(@"C:\Users\Gabriel\source\repos\GitHub\ParallelNetworkProgramming", files, "using");
+                t.Wait();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("-- invalid directory name");
+            }
+        }
+        private static async Task SearchFilesAsync(string dirName, FileInfo[] files, string pattern)
+        {
+            Console.WriteLine($"Files in {dirName} containing '{pattern}'");
+            foreach (var file in files)
+            {
+                var r = new StreamReader(file.FullName);
+                if ((await r.ReadToEndAsync()).IndexOf(pattern, StringComparison.Ordinal) >= 0)
+                {
+                    Thread.Sleep(delay);
+                    Console.WriteLine(file.DirectoryName + " " + file.Name);
+                }
+            }
+        }
+    }
+}
