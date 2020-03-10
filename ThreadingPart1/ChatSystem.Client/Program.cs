6bfa734144354e12998d51856f640c387abcc012
Gabriel Weibel <gabriel.weibel@hotmail.de>
2020-03-10 22:43:25 +0100

    chat

---
+++ ThreadingPart1/ChatSystem.Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatSystem.Client
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var ip = IPAddress.Parse("127.0.0.1");
+            var ipe = new IPEndPoint(ip, 5000);
+            using var client = new TcpClient();
+            client.Connect(ipe);
+            var t = new Thread(ReceiveData) { IsBackground = true };
+            t.Start(client);
+
+            var stream = client.GetStream();
+            while (true)
+            {
+                var s = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(s)) break;
+
+                var buffer = Encoding.ASCII.GetBytes(s);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+            client.Client.Shutdown(SocketShutdown.Send);
+            t.Join();
+        }
+        private static void ReceiveData(object o)
+        {
+            var client = (TcpClient)o;
+            var stream = client.GetStream();
+            var buffer = new byte[1024];
+            while (true)
+            {
+                var bytes = stream.Read(buffer, 0, buffer.Length);
+                if (bytes <= 0) break;
+                
+                var s = Encoding.ASCII.GetString(buffer, 0, bytes);
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ ThreadingPart1/ChatSystem.Server/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatSystem.Server
+{
+    internal static class Program
+    {
+        private static readonly List<TcpClient> Clients = new List<TcpClient>();
+        private static void Main()
+        {
+            var listener = new TcpListener(IPAddress.Any, 5000);
+            listener.Start();
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                lock (Clients)
+                {
+                    Clients.Add(client);
+                }
+
+                Console.WriteLine("Client connected");
+
+                var t = new Thread(HandleClient);
+                t.Start(client);
+            }
+        }
+
+        private static void HandleClient(object o)
+        {
+            var client = (TcpClient)o;
+
+            var buffer = new byte[1024];
+            var stream = client.GetStream();
+            while (true)
+            {
+                var bytes = stream.Read(buffer, 0, buffer.Length);
+
+                if (bytes <= 0) break;
+
+                var data = Encoding.ASCII.GetString(buffer, 0, bytes);
+                Broadcast(data, client);
+            }
+
+            lock (Clients)
+            {
+                Clients.Remove(client);
+            }
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        private static void Broadcast(string data, TcpClient client)
+        {
+            Console.WriteLine(data);
+
+            var buffer = Encoding.ASCII.GetBytes(data);
+            lock (Clients)
+            {
+                Clients.Where(c => c != client).ToList().ForEach(c => c.GetStream()
+                    .Write(buffer, 0, buffer.Length));
+            }
+        }
+    }
+}
